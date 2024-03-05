e827a04d1f013228774303e8dbf5db136869cc9e
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2024-03-06 03:25:14 +0800

    .

--- test/index.php
+++ test/index.php
@@ -1,7 +1,25 @@
 <?php
+require_once __DIR__ . "../../src/Application.php";
 
-require_once __DIR__ . "../../src/Narciso/Application.php";
+use Marcuwynu23\Narciso\Application;
 
-$app = new \Marcuwynu23\Narciso\Application();
-$app->server_log("Hello World");
 
+$app = new Application();
+
+$app->setViewPath(__DIR__ . '/views');
+$app->handleSession();
+$app->handleCors();
+$app->handleDatabase([
+	'type' => 'mysql',
+	'host' => 'localhost',
+	'database' => 'northwind',
+	'username' => 'user',
+	'password' => 'user',
+]);
+
+
+
+$app->route('GET', '/', function () use ($app) {
+	$data = $app->db->query('SELECT * FROM customers')->fetch_all();
+	$app->render('/home.view', $data);
+});
